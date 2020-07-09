16e4fc509056c774188a4a968d7af53a0442deea
Zaneta Grabowska <zaneta_grabowska@poczta.onet.pl>
2020-07-09 05:05:14 +0200

    Merge branches - new values in dict key Warzywniak

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 print("Lista zakupów")
 shopping = {
     "Piekarnia":["Chleb", "Pączek", "Bułki", "Rogale"],
-<<<<<<< HEAD
-    "Warzywniak":["Marchew", "Seler", "Rukola", "Pomidory", "Papryka"],
-=======
     "Warzywniak":["Sałata", "Ogórki", "Cebula"],
->>>>>>> second-branch
     "Sportowy": ["Buty", "Piłka", "Rolki"]
 }
 ilosc_produktow = 0
