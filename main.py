3459b3025beedf4f191600b2931e88cd8601e4b1
Żaneta <67487150+Zaneta6@users.noreply.github.com>
2020-07-09 04:14:23 +0200

    Add new value to dict key "Sportowy" in main.py
    Add new value "Rolki" to dict key "Sportowy" in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Lista zakupów")
 shopping = {
     "Piekarnia":["Chleb", "Pączek", "Bułki", "Rogale"],
     "Warzywniak":["Marchew", "Seler", "Rukola"],
-    "Sportowy": ["Buty", "Piłka"]
+    "Sportowy": ["Buty", "Piłka", "Rolki"]
 }
 ilosc_produktow = 0
 for shop in shopping:
